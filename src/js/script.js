3a6d308c5b4063c8150535086527444ebc3fe336
Victor <barosv@yandex.ru>
2022-03-23 11:25:30 +0300

    Полностью сверстан проект, кроме секции 'quiz'

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+// Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          overlay = document.querySelector('.overlay');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+          
+    function openModal() {
+        overlay.classList.add('show');
+        overlay.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    }
+
+
+    function closeModal() {
+        overlay.classList.add('hide');
+        overlay.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    overlay.addEventListener('click', (e) => {
+        if (e.target === overlay || e.target.getAttribute('data-close') == "") {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code == "Escape" && overlay.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const menu = document.querySelector('.header__nav-menu'),
+          menuItem = document.querySelectorAll('.header__nav-item'),
+          humburger = document.querySelector('.humburger');
+
+    humburger.addEventListener('click', () => {
+        humburger.classList.toggle('humburger_active');
+        menu.classList.toggle('header__nav-menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            humburger.classList.toggle('humburger_active');
+            menu.classList.toggle('header__nav-menu_active');
+        });
+    });
+});
